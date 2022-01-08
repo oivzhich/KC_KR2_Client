d14b9a6125ad1a7f99351ab6ee66f9befaad85e7
Volha Iuzhychenka <Volha_Iuzhychenka@epam.com>
2022-01-08 16:35:15 +0300

    Client: UDP connection

--- KC_KR2_Client/KC_KR2_Client.cpp
+++ KC_KR2_Client/KC_KR2_Client.cpp
@@ -1,20 +1,60 @@
-// KC_KR2_Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#pragma comment (lib, "Ws2_32.lib")
+
+using namespace std;
+
+int main() {
+	//Структура WSADATA содержит информацию о реализации сокетов Windows.
+	WSAData wsaData;
+	WORD wVersionRequested;
+	//----Запрашиваем и загружаем библиотеку------//
+	//Создает значение WORD путем объединения указанных значений.
+	wVersionRequested = MAKEWORD(2, 2);
+	//Функция WSAStartup инициирует использование Winsock DLL процессом.
+	if (WSAStartup(wVersionRequested, &wsaData) != 0) {
+		cout << "Error! Cannot load library!" << endl;
+		return 1;
+	}
+
+	cout << "___CLIENT___" << endl;
+
+	//----Заполняем информацию об адресе сокета---//
+	//Создаем сокет, который привязан к определенному поставщику транспортных услуг.
+	//AF_INET - The Internet Protocol version 4 (IPv4) address family.
+	//SOCK_DGRAM - Тип сокета, который поддерживает дейтаграммы, которые представляют собой 
+	//ненадежные буферы без установления соединения с фиксированной (обычно небольшой) максимальной длиной. 
+	//Этот тип сокета использует протокол дейтаграмм пользователя (UDP) для семейства интернет-адресов (AF_INET или AF_INET6).
+	SOCKET socket1 = socket(AF_INET, SOCK_DGRAM, 0);
+
+	//Структура SOCKADDR_IN определяет транспортный адрес и порт для семейства адресов AF_INET.
+	struct sockaddr_in socketAddress;
+	//Семейство адресов для транспортного адреса. Этот член всегда должен иметь значение AF_INET.
+	socketAddress.sin_family = AF_INET;
+	//Номер порта транспортного протокола.
+	socketAddress.sin_port = htons(1024);
+	//Функция inet_addr преобразует строку, содержащую десятичный адрес IPv4 с точками, в правильный адрес для структуры IN_ADDR .
+	socketAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	char buf[256] = {}, output[256] = {};
+
+	int lengthadd = sizeof(socketAddress);
+	cout << "Enter string: ";
+	cin.getline(buf, 100, '\n');
+
+	cout << "Entered string: " << buf << endl;
+
+	//отправляем данные на сервер
+	sendto(socket1, buf, sizeof(buf), 0, (struct sockaddr*)&socketAddress, sizeof(socketAddress));
+	//получаем данные с сервера и записываем в переменную output
+	recvfrom(socket1, output, sizeof(output), 0, (struct sockaddr*)&socketAddress, &lengthadd);
+
+	cout << "Result: " << output << endl;
+
+	//закрываем соединение
+	closesocket(socket1);
+	WSACleanup();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
